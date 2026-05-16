304d053d307dbd456abad259cc404de5f4453694
joannegton <wtg131998@gmail.com>
2026-05-16 05:37:01 -0300

    feat: altera a estratégia de revalidação para "force-dynamic" na página de projetos

--- src/app/projetos/page.tsx
+++ src/app/projetos/page.tsx
@@ -1,7 +1,7 @@
 import { getProjectRepository } from "@/lib/db"
 import { ProjetosClient } from "@/components/ProjetosClient"
 
-export const revalidate = 60
+export const dynamic = "force-dynamic"
 
 export default async function ProjetosPage() {
   const repo = await getProjectRepository()
